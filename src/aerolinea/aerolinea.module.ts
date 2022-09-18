256a4e58498e66966da4c09895e46b5654963a9b
Manuel Sosa <mr.sosa@uniandes.edu.co>
2022-09-18 15:15:00 -0500

    error modules

--- src/aerolinea/aerolinea.module.ts
+++ src/aerolinea/aerolinea.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AerolineaService } from './aerolinea.service';
 import { AerolineaController } from './aerolinea.controller';
+import { AerolineaEntity } from './aerolinea.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AerolineaEntity])],
   providers: [AerolineaService],
   controllers: [AerolineaController],
 })
--- src/aeropuerto/aeropuerto.module.ts
+++ src/aeropuerto/aeropuerto.module.ts
@@ -1,9 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AeropuertoService } from './aeropuerto.service';
 import { AeropuertoController } from './aeropuerto.controller';
+import { AeropuertoEntity } from './aeropuerto.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AeropuertoEntity])],
   providers: [AeropuertoService],
   controllers: [AeropuertoController],
 })
